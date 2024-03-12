3c609256c3e92ca4a516767f9a07ffef9f618092
giri <girirajlovewanshi@gmail.com>
2024-03-12 19:45:07 +0530

    12 march non primitive data types

---
+++ learnAndFundamentals/conditionals.java
@@ -0,0 +1,11 @@
+public class conditionals {
+    /*
+     * Use if to specify a block of code to be executed, if a specified condition is true
+     * 
+     * Use else to specify a block of code to be executed if the same condition is false
+     * Use else if to specify a new condition to test; if the first condition is false
+     * Use switch to specify many alternative blocks of code to be executed
+     * Use ?,: operator to specify one line condition
+     */
+    
+}
